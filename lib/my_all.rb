13ce33094170085b794e778ddf85e27a1f76bc7b
Adella <adellakenn@gmail.com>
2019-05-15 18:27:03 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0 
   block_return_values = []          # declare our array before entering the while loop
   while i< collection.length 
-    yield(collection[i])            # yield each element in teh collection to the block; sends what is passed in as the argument to the block
+   block_return_values << yield(collection[i])            # yield each element in teh collection to the block; sends what is passed in as the argument to the block
     i += 1
   end
 end
