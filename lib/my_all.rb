e8359fa83c3bcd3958c7afe6e30b6613fb009412
Adella <adellakenn@gmail.com>
2019-05-15 17:57:57 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_all?(collection)
   i = 0 
   while i< collection.length 
-    yield(collection[i])            # yield each element in teh collection to the block 
+    yield(collection[i])            # yield each element in teh collection to the block; sends what is passed in as the argument to the block
     i += 1
   end
 end
