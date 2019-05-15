41d156671b3a1bc6acce7f96486a3caa847af3e9
Adella <adellakenn@gmail.com>
2019-05-15 18:29:09 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,10 +2,16 @@ require 'pry'
 
 def my_all?(collection)
   i = 0 
-  block_return_values = []          # declare our array before entering the while loop
+  block_return_values = []                      # declare our array before entering the while loop
   while i< collection.length 
-   block_return_values << yield(collection[i])            # yield each element in teh collection to the block; sends what is passed in as the argument to the block
+   block_return_values << yield(collection[i])  # yield each element in teh collection to the block; sends what is passed in as the argument to the block
     i += 1
+  end
+  
+  if block_return_values.include?(false)        # Determining the return value
+    false
+  else
+    true
   end
 end
 
@@ -13,4 +19,6 @@ end
 
 # my_all?([1,2,3]) {|i| i < 2}
 
-# 
+# return value is true or false
+
+# block_return_values = [true, false, false]
